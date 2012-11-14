237c44878c1063d20a5619017b51836dd14a089d
Michael Kurze <michael@thefoundation.de>
2012-11-14 18:43:12 +0100

    Added starting resources (chat room).

---
+++ app.js
@@ -0,0 +1,90 @@
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    redis = require('redis'),
+    io = require('socket.io').listen(server);
+
+server.listen(9001);
+
+
+// Initialize Socket.IO
+// http://www.ranu.com.ar/2011/11/redisstore-and-rooms-with-socketio.html
+var port = 6379;
+var pub = redis.createClient(port, "localhost");
+var sub = redis.createClient(port, "localhost");
+var store = redis.createClient(port, "localhost");
+
+io.configure( function(){
+    io.enable('browser client minification');
+    io.enable('browser client etag');
+    io.enable('browser client gzip');
+    io.set('log level', 1);
+    io.set('transports', [
+        'websocket', 'flashsocket', 'htmlfile', 'xhr-polling', 'jsonp-polling'
+    ]);
+    var RedisStore = require('socket.io/lib/stores/redis');
+    io.set('store', new RedisStore({redisPub: pub, redisSub: sub, redisClient: store}));
+});
+
+
+function enc(object) { return JSON.stringify(object) }
+function dec(str)  { return JSON.parse(str) }
+
+var clientsByNick = {};
+
+io.of("/chat").on('connection', function (client) {
+    // client.emit('welcome', { hello: 'world' });
+
+    var nick;
+
+    function clog(title, msg) {
+        console.log("[Client] " + title + " " + enc(msg))
+        client.emit('Log', enc({title: title, payload: msg}))
+    }
+
+    client.on('login', function (msg) {
+        nick = dec(msg).nick;
+
+        if (clientsByNick[nick] != null) {
+            client.emit('Nick Not Allowed', enc({nick: nick, reason: "Already taken!"}));
+            return;
+        }
+
+        if (nick.length < 2) {
+            client.emit('Nick Not Allowed', enc({nick: nick, reason: "Too short!"}));
+            return;
+        }
+
+        client.set('nick', nick, function() {
+            clientsByNick[nick] = client;
+            client.emit('Login', msg);
+            client.broadcast.emit('User Joined', msg);
+
+            var list = [];
+            for (var c in clientsByNick) list.push(c);
+            client.emit('Users', enc({users: list}));
+        });
+
+    });
+
+    client.on('say', function (msg) {
+        var text = dec(msg).text;
+        client.broadcast.emit('Message', enc({text: text, nick: nick}));
+    });
+
+    client.on('whisper', function (msg) {
+        var to = dec(msg).to;
+        var text = dec(msg).text;
+        clientsByNick[to].emit('Private Message', enc({from: nick, text: text}));
+    });
+
+    client.on('disconnect', function () {
+        if (!nick) return;
+        delete clientsByNick[nick];
+        client.broadcast.emit('User Left', enc({nick: nick}));
+    });
+
+});
+
+
+app.use(express.static(__dirname + '/static'));
