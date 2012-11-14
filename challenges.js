209b86e34df15a4c617fa4b7b6de33ddc7855746
Michael Kurze <michael@thefoundation.de>
2012-11-14 20:37:00 +0100

    Started fight implementation.

---
+++ challenges.js
@@ -0,0 +1,52 @@
+var challenges = {
+    "1": {
+        "Du kämpfst wie ein dummer Bauer!":["Wie passend, du kämpfst wie eine Kuh!"]
+    },
+    "2": {
+        "Menschen fallen mir zu Füßen, wenn ich komme!":["Auch bevor sie deinen Atem riechen?"]
+    },
+    "3": {
+        "Ich kenne einige Affen, die mehr drauf haben als du!":["Aha, du warst also beim letzten Familientreffen!"]
+    },
+    "4": {
+        "Meine Narbe im Gesicht stammt aus einem harten Kampf!":["Aha, mal wieder in der Nase gebohrt, wie?"]
+    },
+    "5": {
+        "Du hast die Manieren eines Bettlers!":["Ich wollte, dass du dich wie zuhause fühlst!"]
+    },
+    "6": {
+        "Mein Schwert wird dich aufspießen wie einen Schaschlik!":["Dann mach nicht damit rum wie mit einem Staubwedel!"]
+    },
+    "7": {
+        "Mit meinem Taschentuch werde ich dein Blut aufwischen!":["Also hast du doch den Job als Putze gekriegt!"]
+    },
+    "8": {
+        "Ich hatte mal einen Hund, der war klüger als du!":["Er muß dir das Fechten beigebracht haben!"]
+    },
+    "9": {
+        "Du bist kein echter Gegner für mein geschultes Hirn!":["Vielleicht solltest du es endlich mal benutzen?"]
+    },
+    "10": {
+        "Dein Schwert hat schon bessere Zeiten gesehen!":["Und du wirst deine rostige Klinge nie wieder sehen!"]
+    },
+    "11": {
+        "Niemand wird mich verlieren sehen, du auch nicht!":["Du kannst SO schnell davonlaufen?"]
+    },
+    "12": {
+        "Willst du hören, wie ich 3 Gegner zugleich besiegte?":["Willst du mich mit deinem Geschwafel ermüden?"]
+    },
+    "13": {
+        "Deine Fuchtelei hat nichts mit der Fechtkunst zu tun!":["Doch, doch, du hast sie nur nie gelernt."]
+    },
+    "14": {
+        "Trägst du immer noch Windeln?":["Wieso, die könntest DU viel eher gebrauchen!"]
+    },
+    "15": {
+        "An deiner Stelle würde ich zur Landratte werden!":["Hattest du das nicht vor kurzem getan?"]
+    },
+    "16": {
+        "Jeder hier kennt dich doch als unerfahrenen Dummkopf!":["Zu Schade, dass DICH überhaupt niemand kennt."]
+    }
+};
+
+exports = module.exports = challenges;
